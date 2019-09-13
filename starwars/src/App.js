dfb2544fa03f0c3eb1be4f0ae1ed51347e5b0a06
Nicole Hollis <nicole@emmabeauty.co>
2019-09-13 10:02:47 -0700

    Added styled component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from "../../starwars/node_modules/axios"
 import PeopleCard from "./PeopleCard"
+import LukeCard from "./LukeCard"
 
 const App = () => {
   const [peopleInfo, setpeopleInfo] = useState([]);
@@ -19,13 +20,6 @@ const App = () => {
       });
   }, []);
 
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
@@ -41,7 +35,7 @@ const App = () => {
           />
         )
       })
-      }
+      }   
     </div>
   )
 }
--- starwars/src/PeopleCard.js
+++ starwars/src/PeopleCard.js
@@ -1,17 +1,18 @@
 import React from "react";
 import styled from 'styled-components';
-import App from "./App"
 
+const WhiteH1 = styled.h1`
+color: white;
+font-size: 1.5 rem;
+`;
 const PeopleCard = props => {
     console.log(props)
 
     return (
         <div key={props.index}>
-<h1> Character Name: {props.name}</h1>
-
-</div>
+            <WhiteH1> Character Name: {props.name}</WhiteH1>
+        </div>
     )
 }
-
 
 export default PeopleCard
